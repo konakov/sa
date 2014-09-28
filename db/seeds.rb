596bb82c9cff121092b3e0743e026e5cc0d20c7c
Sergei Konakov <konakov.s@gmail.com>
2014-09-28 21:08:56 +0400

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,21 @@ User.create!(name:  "Example User",
   puts "created user no. #{n+1}"
 end
 
+# Microposts
 users = User.take(6)
 500.times do |n|
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
-  puts "created m-p no. #{n+1} of 50"
+  puts "created m-p no. #{n+1} of 500"
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each do |followed|
+  user.follow(followed)
+  puts "now follow user #{followed}"
+end
+followers.each { |follower| follower.follow(user) }
